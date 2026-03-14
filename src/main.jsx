c66fa3e74981fe44f6080a0eb393a7362d31b6b2
Vera_Facundo <facundovera647@gmail.com>
2026-03-14 16:55:40 -0300

    fix: agrego un responsive general

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/Responsive.css'; // 👈 Agregalo acá abajo
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './context/AuthContext';
